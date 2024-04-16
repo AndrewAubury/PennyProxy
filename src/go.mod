75df3235e60fd0671994522f5a6c7d70d6fbf0f3
Andrew Aubury <andrewaubury1@gmail.com>
2024-04-16 01:25:54 +0100

    Update

--- src/go.mod
+++ src/go.mod
@@ -3,7 +3,7 @@ module PennyProxy
 go 1.19
 
 require (
-	github.com/Tnze/go-mc v1.20.1 // indirect
+	github.com/Tnze/go-mc v1.20.2-0.20240309161106-2445ff3f71f1 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- src/main.go
+++ src/main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"os"
 	"time"
+
+	mcnet "github.com/Tnze/go-mc/net"
+	"github.com/Tnze/go-mc/net/packet"
 
 	"github.com/fatih/color"
 )
 
-type Config struct {
-	ServerAddress string        `json:"serverAddress"`
-	ServerTimeout time.Duration `json:"serverTimeout"`
-	ListenAddress string        `json:"listenAddress"`
-	ListenPort    string        `json:"listenPort"`
+type PterodactylOptions struct {
+	PterodactylURL    string `json:"url"`
+	PterodactylKey    string `json:"apikey"`
+	PterodactylServer string `json:"server"`
 }
 
+type Config struct {
+	ServerAddress string             `json:"serverAddress"`
+	Pterodactyl   PterodactylOptions `json:"pterodactyl"`
+	ServerTimeout time.Duration      `json:"serverTimeout"`
+	ListenAddress string             `json:"listenAddress"`
+	ListenPort    string             `json:"listenPort"`
+}
+
+var pterodactylOptions PterodactylOptions
 var serverIsHealthy bool
+var serverBootRequested bool
 var lastChange time.Time
 
 func readConfig(path string) (*Config, error) {
@@ -78,80 +92,96 @@ func checkServer(address string, timeoutSec time.Duration) bool {
 	return true
 }
 
-// MinecraftPacketHeader creates a packet header for Minecraft server
-func MinecraftPacketHeader(packetID int, data []byte) []byte {
-	var packet []byte
-	// packet = append(packet, byte(len(data)+1)) // Length of packetID + data
-	// packet = append(packet, byte(packetID))    // Packet ID
+// KickPlayer kicks a player if they try to join, telling them to connect later
+func KickPlayerLib(conn mcnet.Conn) error {
 
-	prefix := []byte{0x92, 0x01, 0x00, 0x8f, 0x01}
+	jsonReason := fmt.Sprintf(`{"text":"%s", "color":"red"}`, "A request to turn on the server has been sent, please rejoin in a min")
+	jsonReasonSub := fmt.Sprintf(`{"text":"%s", "color":"red"}`, "The server is booting, please rejoin in a min")
 
-	packet = append(packet, prefix...)
-	packet = append(packet, data...) // Actual data
-	return packet
-}
+	if serverBootRequested {
+		// Prepare the Disconnect packet
+		disconnectPacket := packet.Marshal(
+			0x00, // Packet ID for Disconnect in Login state
+			packet.String(jsonReasonSub),
+		)
+		if err := conn.WritePacket(disconnectPacket); err != nil {
+			return fmt.Errorf("failed to send kick packet: %v", err)
+		}
+	} else {
+		// Prepare the Disconnect packet
+		disconnectPacket := packet.Marshal(
+			0x00, // Packet ID for Disconnect in Login state
+			packet.String(jsonReason),
+		)
+		if err := conn.WritePacket(disconnectPacket); err != nil {
+			return fmt.Errorf("failed to send kick packet: %v", err)
+		}
+		color.Yellow("Sending server boot request")
+		if err2 := startServer(pterodactylOptions.PterodactylKey, pterodactylOptions.PterodactylServer); err2 != nil {
+			color.Red("Server start API call failed: ", err2)
+			return nil
+		}
+		color.Green("Server start API call successed")
+		serverBootRequested = true
 
-// KickPlayer sends a disconnect packet to the player
-func KickPlayer(conn net.Conn) error {
-	// Disconnect message JSON
-	message := `{"text":"Please reconnect later!"}`
-	msgJSON, _ := json.Marshal(message)
-	packet := MinecraftPacketHeader(0x1A, msgJSON) // Disconnect Play Packet ID
-	if _, err := conn.Write(packet); err != nil {
-		return fmt.Errorf("failed to send kick packet: %v", err)
 	}
+
 	return nil
 }
 
-// WriteVarInt writes a Minecraft VarInt to a slice of bytes
-func WriteVarInt(x int) []byte {
-	var buf [5]byte
-	n := 0
-	for x >= 0x80 {
-		buf[n] = byte(x) | 0x80
-		x >>= 7
-		n++
+// SetServerList sets the server list information when players ping the server
+func SetServerListLib(conn mcnet.Conn) error {
+	// Example response for server list
+	serverList := `{
+		"version": {
+			"name": "1.20.4",
+			"protocol": 765
+		},
+		"players": {
+			"max": 100,
+			"online": 0
+		},
+		"description": {
+			"text": "Join to start Server",
+			"color":"red"
+		}
+	}`
+	serverListPacket := packet.Marshal(
+		0x00, // The packet ID for "Server List Response"
+		packet.String(serverList),
+	)
+
+	if err := conn.WritePacket(serverListPacket); err != nil {
+		return fmt.Errorf("failed to set server list: %v", err)
 	}
-	buf[n] = byte(x)
-	return buf[:n+1]
+
+	return nil
 }
 
-// SendPacket sends a Minecraft protocol packet to the connection
-func SendPacket(conn net.Conn, packetID int, data []byte) error {
+func handleLocalPacket(conn mcnet.Conn, p packet.Packet) {
 
-	extraData := []byte{0x8F, 0x01}
-	length := 1 + len(data) + len(extraData)
-	lengthBytes := WriteVarInt(length)
+	//fmt.Printf("p: %v\n", p)
+	if p.Data[len(p.Data)-1] == 0x02 {
+		color.Cyan("Server join recived from %s", conn.Socket.RemoteAddr())
+		fmt.Printf("p: %v\n", p)
+		KickPlayerLib(conn)
+	} else {
+		color.Cyan("Server list ping recived from %s", conn.Socket.RemoteAddr())
+		SetServerListLib(conn)
+	}
 
-	packet := append(lengthBytes, WriteVarInt(packetID)...)
-
-	packet = append(packet, extraData...)
-
-	packet = append(packet, data...)
-
-	_, err := conn.Write(packet)
-	fmt.Printf("%s data: %x\n", "Sending Packet Local", packet)
-	return err
 }
 
-// SetServerList sends a response for the server list query
-func SetServerList(conn net.Conn) error {
-	serverList := `{"version":{"name":"Paper 1.20.4","protocol":765},"enforcesSecureChat":true,"description":"A Minecraft Faker.","players":{"max":20,"online":0}}`
+func handleOfflineClient(conn mcnet.Conn) {
 
-	return SendPacket(conn, 0x00, []byte(serverList)) // Assuming 0x00 is the packet ID for "Server List Response"
-}
-
-func handleOfflineConnection(conn net.Conn) {
-	// Handle the connection
-	if err := SetServerList(conn); err != nil {
+	var p packet.Packet
+	err := conn.ReadPacket(&p)
+	if err != nil {
 		fmt.Println(err)
+		return
 	}
+	handleLocalPacket(conn, p)
 
-	// if err := KickPlayer(conn); err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	conn.Close()
 }
 
 // handleClient handles client connections, proxying data between the client and the server
@@ -185,7 +215,7 @@ func startProxy(listenAddress string, listenPort string, serverAddress string) {
 		if serverIsHealthy {
 			go handleClient(clientConn, serverAddress)
 		} else {
-			go handleOfflineConnection(clientConn)
+			go handleOfflineClient(*mcnet.WrapConn(clientConn))
 		}
 
 	}
@@ -201,6 +231,7 @@ func startHealthCheck(serverAddress string, timeout time.Duration, frequecy time
 			if serverIsHealthy {
 				color.Green("Server health changed to %t at %s, time since last change: %s\n",
 					serverIsHealthy, currentTime.Format(time.RFC3339), currentTime.Sub(lastChange))
+				serverBootRequested = false
 			} else {
 				color.Red("Server health changed to %t at %s, time since last change: %s\n",
 					serverIsHealthy, currentTime.Format(time.RFC3339), currentTime.Sub(lastChange))
@@ -212,6 +243,48 @@ func startHealthCheck(serverAddress string, timeout time.Duration, frequecy time
 	}
 }
 
+// startServer sends a POST request to Pterodactyl Panel to start a server.
+func startServer(apiToken string, serverIdentifier string) error {
+	// Set up the API endpoint. Replace "your-panel-url.com" with your actual panel URL.
+	url := fmt.Sprintf("%s/api/client/servers/%s/power", pterodactylOptions.PterodactylURL, serverIdentifier)
+
+	// Create the JSON body for the request
+	var jsonData = []byte(`{"signal":"start"}`)
+
+	// Create a new request using http
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("error creating request: %v", err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiToken)
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request to server: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read and print the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body: %v", err)
+	}
+
+	if false {
+		fmt.Printf("Response from server: %s\n", string(body))
+	}
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("failed to start server, status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
 func main() {
 	config, err := readConfig("config.json")
 
@@ -220,6 +293,7 @@ func main() {
 		os.Exit(1)
 	}
 	lastChange = time.Now()
+	pterodactylOptions = config.Pterodactyl
 	go startProxy(config.ListenAddress, config.ListenPort, config.ServerAddress)
 	go startHealthCheck(config.ServerAddress, config.ServerTimeout, 1)
 
